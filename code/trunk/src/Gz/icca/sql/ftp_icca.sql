1eb3871632a944c3ad58422579f61bedf6d44349
Administrator <Administrator@a5f90e8c-3a3f-1f4f-b7df-93df2517f620>
2012-03-16 07:00:27 +0000

    20120316
    git-svn-id: svn://182.53.35.141/ICS4.0@58 a5f90e8c-3a3f-1f4f-b7df-93df2517f620

--- code/trunk/src/Gz/icca/sql/ftp_icca.sql
+++ code/trunk/src/Gz/icca/sql/ftp_icca.sql
@@ -1,4 +1,4 @@
 --delete from pubftpcfg where ftpid = 'GZ_ICSCH01';
+--delete from pubftpcfg where ftpid = 'GZ_ICSCH02';
 --insert into pubftpcfg values('441999','GZ_ICSCH01','137.28.45.27','bank_boc','boc2010!@#','./','$SndFil','dat/icca','$LclFil','asc',' ','广州大学城IC联名卡对账文件上传');
-
-
+--insert into pubftpcfg values('441999','GZ_ICSCH02','182.53.13.13','gy','gy','./','$SndFil','dat/icca','$FilNam','asc',' ','电子现金圈存对账文件格式自动触发');
