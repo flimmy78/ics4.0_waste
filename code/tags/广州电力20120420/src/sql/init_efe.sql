57d8839b1e624dd332fedc3a1e0c18b61c620dec
Administrator <Administrator@a5f90e8c-3a3f-1f4f-b7df-93df2517f620>
2012-04-20 11:33:45 +0000

    git-svn-id: svn://182.53.35.141/ICS4.0@106 a5f90e8c-3a3f-1f4f-b7df-93df2517f620

---
+++ code/tags/广州电力20120420/src/sql/init_efe.sql
@@ -0,0 +1,22 @@
+--添加电子柜员
+insert into pubdumtlr values('441999','EFE','','EEFEBI2');
+
+
+
+--delete from pubaplbus where brno='441999' and  APLCLS='202' and bustyp='EFEA1';
+insert into pubaplbus values('441999','202','','EFEA1','广东电力代收费');
+--单位代码对照表(应用码、应用子码、业务类型、业务类型描述)
+
+--delete from pubaplmng where brno='441999' and  APLCLS='202' and bustyp='EFEA1';
+insert into pubaplmng values('441999','202','EFEA1','','9999999999');
+insert into pubaplmng values('441999','202','CRP51','01','9999999999');
+--应用管理表(应用码、业务类型、业务子码、单位代码)
+
+--delete from pubjnldef where brno='441999' and bustyp='EFEA1';
+insert into pubjnldef values('441999','EFEA1','9999999999','afetxnjnl','afetxnjnl','1','1');
+--pubjnldef业务流水定义管理表(业务类型、、联机流水表名、批量流水表名、联机、批量对帐标记)
+
+--增加清算参数
+insert into efeaclrtbl values('441999', '4410003976','20120421','1800','0','1');
+insert into efeaclrtbl values('441999', '4410003977','20120421','1800','0','1');
+insert into efeaclrtbl values('441999', '4410003978','20120421','1800','0','1');
