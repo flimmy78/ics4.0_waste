4a5f814fc51ce6e3fddd4edb432fd130a92ff9cb
Administrator <Administrator@a5f90e8c-3a3f-1f4f-b7df-93df2517f620>
2012-12-12 09:11:01 +0000

    修改初始化参数
    git-svn-id: svn://182.53.35.141/ICS4.0@187 a5f90e8c-3a3f-1f4f-b7df-93df2517f620

---
+++ code/branches/ycta/test/src/sql/ftp_ycta.sql
@@ -0,0 +1,6 @@
+--delete from pubftpcfg where ftpid = 'YCT_JYF_441';
+--delete from pubftpcfg where ftpid = 'YCT_CZF_441';
+--delete from pubftpcfg where ftpid = 'YCT_RZF_441';
+insert into pubftpcfg values('441999','YCT_JYF_441','182.53.13.13','gy','gy','./','$JYFile','dat/ycta','$JYFile','asc',' ','羊城通成功文件');
+insert into pubftpcfg values('441999','YCT_CZF_441','182.53.13.13','gy','gy','./','$CZFile','dat/ycta','$CZFile','asc',' ','羊城通成功文件');
+insert into pubftpcfg values('441999','YCT_RZF_441','182.53.13.13','gy','gy','./','$LclFil','dat/ycta','$LclFil','asc',' ','羊城通审计文件');
