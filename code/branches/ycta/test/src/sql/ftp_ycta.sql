a536b0dcf9fb82bfac7deeb7e5831e2ff2155ad7
Administrator <Administrator@a5f90e8c-3a3f-1f4f-b7df-93df2517f620>
2012-12-21 09:44:48 +0000

    羊城通生产配置更新
    git-svn-id: svn://182.53.35.141/ICS4.0@196 a5f90e8c-3a3f-1f4f-b7df-93df2517f620

--- code/branches/ycta/test/src/sql/ftp_ycta.sql
+++ code/branches/ycta/test/src/sql/ftp_ycta.sql
@@ -1,6 +1,6 @@
 --delete from pubftpcfg where ftpid = 'YCT_JYF_441';
 --delete from pubftpcfg where ftpid = 'YCT_CZF_441';
 --delete from pubftpcfg where ftpid = 'YCT_RZF_441';
-insert into pubftpcfg values('441999','YCT_JYF_441','182.53.13.13','gy','gy','./','$JYFile','dat/ycta','$JYFile','asc',' ','羊城通成功文件');
-insert into pubftpcfg values('441999','YCT_CZF_441','182.53.13.13','gy','gy','./','$CZFile','dat/ycta','$CZFile','asc',' ','羊城通成功文件');
-insert into pubftpcfg values('441999','YCT_RZF_441','182.53.13.13','gy','gy','./','$LclFil','dat/ycta','$LclFil','asc',' ','羊城通审计文件');
+insert into pubftpcfg values('441999','YCT_JYF_441','10.240.1.7','jh','jh123','./detail','$JYFile','dat/ycta','$JYFile','asc',' ','羊城通交易文件');
+insert into pubftpcfg values('441999','YCT_CZF_441','10.240.1.7','jh','jh123','./detail','$CZFile','dat/ycta','$CZFile','asc',' ','羊城通冲正文件');
+insert into pubftpcfg values('441999','YCT_RZF_441','10.240.1.7','jh','jh123','./detail','$LclFil','dat/ycta','$LclFil','asc',' ','羊城通审计文件');
