e0604fa157bc12f6991c42d276b01a2c3f1f6a41
Qiming Gu <cruiser31@gmail.com>
2013-12-12 11:10:40 +0800

    code_complete:tbc惠州烟草开发测试完毕

--- code/trunk/src/Gz/tbc/sql/init_tbc_hz.sql
+++ code/trunk/src/Gz/tbc/sql/init_tbc_hz.sql
@@ -4,8 +4,14 @@
 --/////////////////////
 
 --添加电子柜员
---测试，EPA0DT1-EPA0DT5
-insert into pubdumtlr values('491999','TBC','','EPA0DT1');
+insert into pubdumtlr values('491999','TBC','','EYYTDT1');
 
 --添加烟草基础信息表
 insert into tbcbasinf values('491999','5950','惠州市公司',' ',' ','20111114','5950','0');
+
+insert into pubaplbus values('491999','438','','TBCA1','广东烟草公司');
+
+insert into pubaplmng values('491999','438','TBCA1','','9999999999');
+insert into pubaplmng values('491999','438','CRP51','01','9999999999');
+
+insert into pubjnldef values('491999','TBCA1','9999999999','afetxnjnl','afetxnjnl','1','1');
