ee3981d7b731b89e4f8b0568146a6857773c6f3d
Qiming Gu <cruiser31@gmail.com>
2014-11-29 11:11:22 +0800

    开发sftp上传程序

---
+++ code/trunk/app/efek/bin/EfeFilSend.sh
@@ -0,0 +1,23 @@
+# $1: 远程主机IP 182.53.201.46
+# $2: 远程主机用户 bcm
+# $3: 远程主机目录 exchange
+# $4: 本地文件目录 /app/ics/app/efek/tst/
+# $5: 本地文件名 HDXY03010301121520141125001.test
+
+if [ $# -ne 5 ]
+then
+    echo "Usage: $0 ip username remote-dir local-dir local-file\n"
+    echo "Usage: $0 182.53.201.46 bcm exchange /app/ics/app/efek/tst/ HDXY03010301121520141125001.test\n"
+    exit -1
+fi
+
+
+
+echo "cd $3" > /app/ics/app/efek/dat/ftpTmp
+echo "lcd $4"      >> /app/ics/app/efek/dat/ftpTmp
+echo "-put $5"     >> /app/ics/app/efek/dat/ftpTmp
+echo "quit"        >> /app/ics/app/efek/dat/ftpTmp
+
+sftp -b /app/ics/app/efek/dat/ftpTmp $2@$1
+
+exit 0
---
+++ code/trunk/app/efek/bin/GeneMd5.sh
@@ -0,0 +1,12 @@
+# $1: 需要计算MD5值的文件
+# 输出:将计算结果放置在/app/ics/efek/bin/md5文件里面
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 filename \n"
+    exit -1
+fi
+
+cat $1 |openssl md5 > /app/ics/efek/bin/md5
+
+exit 0
